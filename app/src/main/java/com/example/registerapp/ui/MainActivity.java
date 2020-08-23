2dbb6a18a276dfe93caf0037fc44da2b33374117
AxelRocha <oaoexoufpr@gmail.com>
2020-08-23 14:56:01 -0300

    creating MainActivity layout

--- app/src/main/java/com/example/registerapp/ui/MainActivity.java
+++ app/src/main/java/com/example/registerapp/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.registerapp;
+package com.example.registerapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.registerapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
