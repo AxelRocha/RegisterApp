6f821a8962cf4648a9124c20bed7a7977403b790
AxelRocha <oaoexoufpr@gmail.com>
2020-08-23 16:49:24 -0300

    creating add register activity

---
+++ app/src/main/java/com/example/registerapp/ui/addregister/AddRegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.registerapp.ui.addregister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.registerapp.R;
+
+public class AddRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_register);
+    }
+}
